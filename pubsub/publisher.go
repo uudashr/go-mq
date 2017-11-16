8a1149530937bb05234e4d4cace3f34f9f22bc79
Nuruddin Ashr <uudashr@gmail.com>
2017-11-16 15:56:52 +0700

    Fix the pubsub publisher

--- pubsub/publisher.go
+++ pubsub/publisher.go
@@ -2,6 +2,7 @@ package pubsub
 
 import (
 	"context"
+	"sync"
 
 	"cloud.google.com/go/pubsub"
 
@@ -11,35 +12,53 @@ import (
 // Publisher publish message.
 type Publisher struct {
 	client *pubsub.Client
-	topic  *pubsub.Topic
+
+	mu     sync.Mutex
+	topics map[string]*pubsub.Topic
 }
 
 // Publish message body to a topic.
 func (p *Publisher) Publish(topic string, body []byte) error {
-	res := p.topic.Publish(context.Background(), &pubsub.Message{
+	pub := p.topic(topic)
+	res := pub.Publish(context.Background(), &pubsub.Message{
 		Data: body,
 	})
 	_, err := res.Get(context.Background())
 	return err
 }
 
+func (p *Publisher) topic(id string) *pubsub.Topic {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	topic := p.topics[id]
+	if p == nil {
+		topic = p.client.Topic(id)
+		p.topics[id] = topic
+	}
+	return topic
+}
+
 // Stop the publisher.
 func (p *Publisher) Stop() error {
-	p.topic.Stop()
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	for _, topic := range p.topics {
+		topic.Stop()
+	}
+
 	return p.client.Close()
 }
 
 // NewPublisher constructs new Publisher.
-func NewPublisher(projectID, topicID string, opts ...option.ClientOption) (*Publisher, error) {
+func NewPublisher(projectID string, opts ...option.ClientOption) (*Publisher, error) {
 	client, err := pubsub.NewClient(context.Background(), projectID, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	topic := client.Topic(topicID)
-
 	return &Publisher{
 		client: client,
-		topic:  topic,
+		topics: make(map[string]*pubsub.Topic),
 	}, nil
 }
